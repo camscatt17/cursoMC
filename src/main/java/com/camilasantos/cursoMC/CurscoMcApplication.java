ab1c9b5cfaf29dc2a37c5f30f25d8c595d425956
CamilaSantos <camscatt17@gmail.com>
2020-05-05 23:03:25 -0300

    Projeto criado

---
+++ src/main/java/com/camilasantos/cursoMC/CurscoMcApplication.java
@@ -0,0 +1,13 @@
+package com.camilasantos.cursoMC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurscoMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurscoMcApplication.class, args);
+	}
+
+}
