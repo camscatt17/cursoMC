ebe74c18b3342a2708bd431dde7b8d699fd5e35f
CamilaSantos <camscatt17@gmail.com>
2020-05-14 17:24:40 -0300

    Estado e cidade

--- src/main/java/com/camilasantos/cursoMC/CurscoMcApplication.java
+++ src/main/java/com/camilasantos/cursoMC/CurscoMcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.camilasantos.cursoMC.domain.Categoria;
+import com.camilasantos.cursoMC.domain.Cidade;
+import com.camilasantos.cursoMC.domain.Estado;
 import com.camilasantos.cursoMC.domain.Produto;
 import com.camilasantos.cursoMC.repositories.CategoriaRepository;
+import com.camilasantos.cursoMC.repositories.CidadeRepository;
+import com.camilasantos.cursoMC.repositories.EstadoRepository;
 import com.camilasantos.cursoMC.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CurscoMcApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurscoMcApplication.class, args);
@@ -41,8 +51,23 @@ public class CurscoMcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		Estado est1 = new Estado (null, "Minas Gerais");
+		Estado est2 = new Estado (null, "Sao Paulo");
+				
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Cidade c1 = new Cidade (null, "Uberlandia", est1);
+		Cidade c2 = new Cidade (null, "Sao Paulo", est2);
+		Cidade c3 = new Cidade (null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		
 	}
 	
 	
